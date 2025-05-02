65551b4b8bf79c65ba2850e53b76ed10b7270faf
jonatan-esprella-lopez <jonatanesprellalopez@gmail.com>
2025-05-02 00:22:39 -0400

    .\.git\

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,6 @@ dependencyResolutionManagement {
 
 rootProject.name = "Esqueleto_Mobile"
 include(":app")
- 
+include(":data")
+include(":domain")
+include(":usesCases")
---
+++ usesCases/src/main/java/com/example/usescases/MyClass.kt
@@ -0,0 +1,4 @@
+package com.example.usescases
+
+class MyClass {
+}
